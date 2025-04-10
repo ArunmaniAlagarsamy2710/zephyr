80ce0cfba877a8f321eee7258523315e81a4fe2d
Arunmani Alagarsamy <arunmani.a@silabs.com>
2025-04-10 16:33:12 +0530

    drivers: wifi: siwx91x: Enable AP configuration support
    This patch adds support for configuring AP. It enables setting
    key parameters before AP mode is enabled.
    
    - Adds support for configuring client maximum inactivity timeout.
    - Adds support for bandwidth, It supports 20MHZ only.
    - Adds support for setting the maximum number of clients and
      hidden SSID mode by rebooting the NWP device.
    
    Signed-off-by: Arunmani Alagarsamy <arunmani.a@silabs.com>

--- drivers/wifi/siwx91x/siwx91x_wifi.h
+++ drivers/wifi/siwx91x/siwx91x_wifi.h
@@ -26,6 +26,9 @@ struct siwx91x_dev {
 	sl_si91x_performance_profile_t exit_strategy_mode;
 	uint16_t bcn_listen_interval;
 	bool listen_interval_wakeup;
+	uint8_t max_num_sta;
+	bool reboot_needed;
+	bool hidden_ssid;
 
 #ifdef CONFIG_WIFI_SILABS_SIWX91X_NET_STACK_OFFLOAD
 	struct k_event fds_recv_event;
